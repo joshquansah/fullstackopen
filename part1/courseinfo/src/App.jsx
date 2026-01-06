b239978a5b2340433b0b67bcdef4ae489dbfddce
Your Name <241776793+joshquansah@users.noreply.github.com>
2026-01-05 22:32:02 -0500

    p

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,17 +1,29 @@
-const Header = (props) =>{
-  console.log(props)
+const Header = ({course}) =>{
   return (
     <>
-    <h1>{props.course}</h1>
+    <h1>{course.name}</h1>
     </>
   )
 }
 
-const Content = ({parts}) => {
-  console.log(parts)
-  return(
-    parts.map(part => (<p key = {part.name}>{part.name} {part.exercises}
-      </p>))
+const Content = ({course}) => 
+  <div>
+    <p>
+    {course.name} {course.exercises}
+    </p>
+  </div>
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course} />
+      
+      {course.parts.map(part => 
+      <Content key={part.id} course = {part} />
+      )}
+      
+    </div>
+    
   )
 }
 const Total = ({parts}) => {
@@ -25,19 +37,23 @@ const Total = ({parts}) => {
 }
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
@@ -45,8 +61,7 @@ const App = () => {
 
   return (
     <div>
-      <Header course = {course.name} />
-      <Content parts = {course.parts} />
+      <Course course={course} />
       <Total parts = {course.parts} />
     </div>
   )
